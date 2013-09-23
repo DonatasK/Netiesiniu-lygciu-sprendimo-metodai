5c9d0c121bb1a8ac74c0a61db4e82072b1a65c46
donatask4 <donatask4@gmail.com>
2013-09-23 12:13:44 +0300

    papildziau komentarais

--- pusiaukirtosmetodas.m
+++ pusiaukirtosmetodas.m
@@ -2,22 +2,25 @@ function saknis = pusiaukirtosmetodas(func,x1,x2,filter,tol)
 % Pusiaukirtos metodu surandamas funkcijos f(x) nulis intervale (x1,x2).
 % IVEDIMO PARAMETRAI:
 % filter = singuliarumo filtras: 0 = iðjungta (pagal nutylejima), 1 = ijungta.
-% tol = paklaida (pagal nutylejima 0.0001).
-if nargin < 5; tol = 0.0001; end
-if nargin < 4; filter = 0; end
+% tol = paklaida (pagal nutylejima 0.001).
+%
+if nargin < 5; tol = 0.001; end % nargin nustato kiek ivesta duomenu
+if nargin < 4; filter = 0; end % naudojama jai vartotojas nesurase reikamu duomenu
+%
 f1 = func(x1);
-if f1 == 0.0; saknis = x1; return; end
+if f1 == 0.0; saknis = x1; return; end % tikrina ar f1 = 0
 f2 = func(x2);
-if f2 == 0.0; saknis = x2; return; end
+if f2 == 0.0; saknis = x2; return; end % tikrina ar f2 = 0
 if f1*f2 > 0;
-error('Nurodytame intervale (x1,x2) nera funkcijos nulio')
+error('Nurodytame intervale (x1,x2) nera funkcijos nulio') % error nutraukia fukcija ir pateikia paaiskinima
 end
-n = ceil(log2(abs(x2 - x1)/tol));
+%
+n = ceil(log2(abs(x2 - x1)/tol)); % ceil apvalina skaicius
 for i = 1:n
     x3 = 0.5*(x1 + x2);
     f3 = func(x3);
     if (filter == 1) && (abs(f3) > abs(f1)) && (abs(f3) > abs(f2))
-        saknis = NaN; return
+        saknis = NaN; return % NaN ne numeris
     end
     if f3 == 0.0
         saknis = x3; return
